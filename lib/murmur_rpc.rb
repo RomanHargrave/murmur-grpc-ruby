63bd2e56fccbeeca3bbf032e5c53b6c5b59429d2
Roman Hargrave <roman@hargrave.info>
2020-11-29 23:06:07 -0600

    Remove MurmurRPC module documentation from source

--- lib/murmur_rpc.rb
+++ lib/murmur_rpc.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
+
 # Murmur RPC client for Ruby
 # (C) 2020- Roman Hargrave <roman@hargrave.info>
 # Licensed under the GPL-3.0 - see LICENSE for more details
 
-require 'murmur_rpc/murmur_rpc_pb'
-require 'murmur_rpc/murmur_rpc_services_pb'
-
-# Murmur RPC client
-module MurmurRPC
-end
+require_relative 'murmur_rpc/murmur_rpc_pb'
+require_relative 'murmur_rpc/murmur_rpc_services_pb'
